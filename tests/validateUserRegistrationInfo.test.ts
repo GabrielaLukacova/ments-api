a9622be4c48e4b6c40e2b8bfd2ad1c54a4f20904
GabrielaLukacova <gabluk01@easv365.dk>
2025-04-08 22:11:33 +0200

    test for empty name

--- tests/validateUserRegistrationInfo.test.ts
+++ tests/validateUserRegistrationInfo.test.ts
@@ -2,24 +2,6 @@ import { validateUserRegistrationInfo } from "../src/controllers/authController"
 import { User } from "../src/interfaces/user";
 import mongoose from "mongoose";
 
-// test valid registration
-test("Valid registration info should pass", () => {
-    // Arrange
-    const validUser = {
-        id: new mongoose.Types.ObjectId().toHexString(),
-        name: "Lara Johnson",
-        email: "lara.johnson@example.com",
-        password: "SecurePass123!",
-        registerDate: new Date()
-    } as unknown as User;
-
-    // Act
-    const { error } = validateUserRegistrationInfo(validUser);
-
-    // Assert
-    expect(error).toBeUndefined(); // No error should occur for valid input
-});
-
 // test for empty name
 test("Empty name should fail validation", () => {
     // Arrange
